3d7a07b78e133994d86d3004d0537bddd25e0b0a
Ross, Andrew <AD92242@ad.wellpoint.com>
2026-09-25 09:24:40 -0500

    refactor: merge ProcessStepDefinition.cs, ProcessStepDependencyGraph.cs, and ParticipantRegistryItem.cs into their registry files
    - ProcessStepDefinition + ProcessStepTypeDefinition moved into ProcessStepRegistry.cs
      (internal types only ever used there)
    - ProcessStepDependencyGraph moved into ProcessStepRegistry.cs
    - ProcessorRegistryItem and related record types moved into ProcessRegistry.cs
      (they describe the registry output and are only referenced by ProcessRegistry)

--- src/Kaleido/Process/Registry/ProcessRegistry.cs
+++ src/Kaleido/Process/Registry/ProcessRegistry.cs
@@ -2,6 +2,92 @@ using Microsoft.Extensions.Logging;
 
 namespace Kaleido.Process.Registry;
 
+[ExcludeFromCodeCoverage]
+public record ProcessorRegistryItem
+{
+    /// <summary>
+    /// Marks this processor as the entry point for the application workflow.
+    /// When true, consumers should start with this processor.
+    /// Only one processor in a distributed system should have this set to true.
+    /// </summary>
+    public bool IsEntryProcessor { get; init; }
+
+    public IReadOnlyCollection<ProcessorStepSummary> InitialSteps { get; init; }
+        = [];
+
+    public IReadOnlyCollection<ProcessorStepRegistryItem> Steps { get; init; }
+        = [];
+}
+
+[ExcludeFromCodeCoverage]
+public record ProcessorStepRegistryItem
+{
+    public required string Name { get; init; }
+
+    public string? Description { get; init; }
+
+    public string? DisplayName { get; init; }
+
+    public string? Version { get; init; }
+
+    public bool Repeatable { get; init; }
+
+    public IReadOnlyCollection<ProcessorInputFieldDescriptor> Fields { get; init; }
+        = [];
+
+    public IReadOnlyCollection<ProcessorStepSummary> Dependencies { get; init; }
+        = [];
+
+    public IReadOnlyCollection<ProcessorStepSummary> AvailableAfter { get; init; }
+        = [];
+
+    public IReadOnlyCollection<ProcessorStepSummary> AvailableUntil { get; init; }
+        = [];
+
+    public ProcessorStepResultDescriptor? Result { get; init; }
+}
+
+[ExcludeFromCodeCoverage]
+public record ProcessorStepSummary
+{
+    public required string Name { get; init; }
+
+    public string? Description { get; init; }
+
+    public string? DisplayName { get; init; }
+
+    public string? Version { get; init; }
+
+    public bool Repeatable { get; init; }
+}
+
+[ExcludeFromCodeCoverage]
+public record ProcessorPropertyDescriptor
+{
+    public required string Name { get; init; }
+
+    public string? Description { get; init; }
+
+    public required DataTypeDescriptor DataType { get; init; }
+}
+
+[ExcludeFromCodeCoverage]
+public record ProcessorInputFieldDescriptor : ProcessorPropertyDescriptor
+{
+    public IReadOnlyCollection<ConstraintContract> Constraints { get; init; }
+        = [];
+}
+
+[ExcludeFromCodeCoverage]
+public record ProcessorOutputFieldDescriptor : ProcessorPropertyDescriptor;
+
+[ExcludeFromCodeCoverage]
+public record ProcessorStepResultDescriptor
+{
+    public IReadOnlyCollection<ProcessorOutputFieldDescriptor> OutputFields { get; init; }
+        = [];
+}
+
 public interface IProcessRegistry
 {
     IReadOnlyCollection<ProcessorRegistryItem> Registrations { get; }
